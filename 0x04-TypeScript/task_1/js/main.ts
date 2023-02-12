d97b1658c7e76afbe7420e59d155ff23d3f282eb
meyota <meyota08@gmail.com>
2023-02-12 21:05:28 +0000

    Let build a Teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,4 +17,31 @@ const teacher3: Teacher = {
   contract: false,
 };
 
-console.log(teacher3);
+console.log(teacher3)i;
+
+// -----------------Task 2--------------------
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// -----------------Task 3---------------------
+
+interface printTeacherFunction {
+  firstName: string;
+  lastName: string;
+}
+function printTeacher(firstName: string, lastName: string): string {
+  let firstLetter: string = firstName.charAt(0);
+  return(`${firstLetter}.${lastName}`)
+}
+
+console.log(printTeacher("John", "Doe"));
